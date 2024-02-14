eaf0ba5dadfce99edeb7fee5c725e713a9a1f5f6
zGiusee <giusebelforte@gmail.com>
2024-02-14 14:05:03 +0100

    [UPDATE] controller collegato correttamente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MovieController as MovieController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'index'])->name('home');
